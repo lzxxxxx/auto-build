86c7eb7912c67c8e2035e73889548a01aa7b9938
liuzixu <liuzixu@xiaomi.com>
2018-02-22 16:56:55 +0800

    fsd

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ handler.on('push', function (event) {
 function runCommand( cmd, args, callback ){
     var child = spawn( cmd, args );
     var response = '';
-    child.stdout.on('data', function( buffer ){ resp += buffer.toString(); });
-    child.stdout.on('end', function(){ callback( resp ) });
+    child.stdout.on('data', function( buffer ){ response += buffer.toString(); });
+    child.stdout.on('end', function(){ callback( response ) });
 }
 // 由于我们不需要监听issues，所以下面代码注释掉
 //  handler.on('issues', function (event) {
